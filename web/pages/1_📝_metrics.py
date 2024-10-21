1f2505878c987cf4ea5fe4a20c6b4cba0a7dad53
filvaleriia <61471458+filvaleriia@users.noreply.github.com>
2024-10-21 09:40:39 +0000

    s

--- web/pages/1_📝_metrics.py
+++ web/pages/1_📝_metrics.py
@@ -22,12 +22,12 @@ st.markdown(
     #### **SESY** 
     > this metric is about how diverse our virtual library is. It is important for showing which generator has more different compounds in the output.
 
-    | NAME | | |
+    | NAME | FORMULA | LIMITATIONS |
     |-------------|-|-|
-    |▪ True positive recall all (TUPOR_old) – | $$TUPOR_old = {{UASo} \over UASr}$$ | |
-    |▪ True positive recall all (TUPOR) – | $$TUPOR = {{UASo*10000} \over (USo * UASr)}$$ | |
-    |▪ Set scaffold yield (SESY) –| $$SESY = {{USo} \over SSo}$$ | |
-    |▪ Absolute set scaffold recall (ASER) –| $$ASER = {{CwASo} \over SSo}$$ | |
+    |▪ True positive recall all (TUPOR_old) – | $$TUPORold = {{UASo} \over UASr}$$ | [0 ; 1] |
+    |▪ True positive recall all (TUPOR) – | $$TUPOR = {{UASo*10000} \over (USo * UASr)}$$ | [0 ; 10 000 \over USo] |
+    |▪ Set scaffold yield (SESY) –| $$SESY = {{USo} \over SSo}$$ | [0 ; 1] |
+    |▪ Absolute set scaffold recall (ASER) –| $$ASER = {{CwASo} \over SSo}$$ |[0 ; 1] |
  
 
     * UASo - unique active scaffolds in cOS
